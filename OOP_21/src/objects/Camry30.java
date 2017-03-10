defb71d80089a75c3fa5e6ea66a27196660ce18e
Igor <zolotov.igor@gmail.com>
2017-03-10 13:34:52 +0300

    add OOP_21 (class extends)

---
+++ OOP_21/src/objects/Camry30.java
@@ -0,0 +1,15 @@
+package objects;
+
+
+public class Camry30 {
+
+    public void drive(){
+        System.out.println("Camry30 drive");
+
+    }
+
+    public void stop(){
+        System.out.println("Camry30 stop");
+
+    }
+}
---
+++ OOP_21/src/start/Main.java
@@ -0,0 +1,14 @@
+package start;
+
+import objects.Camry50;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Camry50 camry50 = new Camry50();
+        camry50.drive();
+        camry50.stop();
+
+    }
+
+}
